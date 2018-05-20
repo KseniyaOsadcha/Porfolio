ae4576f9345e36d2afc7a7180585fd126a7d0503
KseniyaOsadcha <theksenic@gmail.com>
2018-05-20 13:44:43 +0300

    add modal window

--- scripts/scrolling.js
+++ scripts/scrolling.js
@@ -1,4 +1,4 @@
-$(function () {
+$( document ).ready(function () {
     var $page = $('html, body');
     $('a[href*="#"]').click(function() {
         $page.animate({
--- scripts/why-me-slide.js
+++ scripts/why-me-slide.js
@@ -1,4 +1,4 @@
-$(function () {
+$( document ).ready(function () {
     console.log('w-m-box-1');
     $('._w-paragraph-1').click(function () {
         $('._w-m-box-1').slideToggle();
